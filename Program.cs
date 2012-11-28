5d7fec60e58114abbfedc8fcdf7560e4dbb9ceba
Damien Lawford <killerdamo@virginmedia.com>
2012-11-28 03:34:45 +0000

    patch 1.5.3

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace YGOPro_Launcher
     static class Program
     {
 
-        public const string Version = "152200";
+        public const string Version = "153000";
         public static Configuration Config;
         public static LanguageManager LanguageManager;
         public static NetClient ServerConnection;
