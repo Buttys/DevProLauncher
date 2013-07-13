21ed40b4a9db97091a0f4fb5d9701d6c3b88b85e
sidschingis <sijing@gmx.net>
2013-07-13 14:59:09 +0200

    Create c89642993.lua
    Number 63: Shamoji Soldier

---
+++ script/c89642993.lua
@@ -0,0 +1,64 @@
+--Ｎｏ．６３ おしゃもじソルジャー
+function c89642993.initial_effect(c)
+  --xyz summon
+	aux.AddXyzProcedure(c,aux.XyzFilterFunction(c,1),2)
+	c:EnableReviveLimit()
+	--tograve
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(89642993,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(c89642993.cost)
+	e1:SetTarget(c89642993.drtg)
+	e1:SetOperation(c89642993.drop)
+	c:RegisterEffect(e1)
+	--Recover
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(89642993,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCost(c89642993.cost)
+	e2:SetTarget(c89642993.retg)
+	e2:SetOperation(c89642993.reop)
+	c:RegisterEffect(e2)
+end
+function c89642993.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(89642993)==0
+		and e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RegisterFlagEffect(89642993,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
+	Duel.Hint(HINT_OPSELECTED,1-tp,e:GetDescription())
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+
+function c89642993.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) and Duel.IsPlayerCanDraw(1-tp,1) end
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,PLAYER_ALL,1)
+end
+function c89642993.drop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e1:SetCountLimit(1)
+	e1:SetReset(RESET_EVENT+0x2fe0000+RESET_PHASE+PHASE_STANDBY)
+	e1:SetOperation(c89642993.activate)
+	Duel.RegisterEffect(e1,1)
+end
+
+function c89642993.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_CARD,0,89642993)
+	Duel.Draw(tp,1,REASON_EFFECT)
+	Duel.Draw(1-tp,1,REASON_EFFECT)
+end
+
+function c89642993.retg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,PLAYER_ALL,1000)
+end
+function c89642993.reop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Recover(tp,1000,REASON_EFFECT)
+	Duel.Recover(1-tp,1000,REASON_EFFECT)
+end
