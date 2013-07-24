99e0c77ce1cdd95979207d393d1e96271bcc6809
sidschingis <sijing@gmx.net>
2013-07-24 14:42:26 +0200

    fix

--- script/c77631175.lua
+++ script/c77631175.lua
@@ -10,25 +10,43 @@ function c77631175.initial_effect(c)
 	e1:SetCode(EFFECT_DESTROY_REPLACE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTarget(c77631175.reptg)
-	e1:SetOperation(c77631175.atkop)
+	e1:SetOperation(c77631175.repop)
 	c:RegisterEffect(e1)
+	--atkup & damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetCode(77631175)
+	e2:SetTarget(c77631175.atktg)
+	e2:SetOperation(c77631175.atkop)
+	c:RegisterEffect(e2)
 end
 function c77631175.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_EFFECT) and e:GetHandler():IsReason(REASON_BATTLE) end
 	if Duel.SelectYesNo(tp,aux.Stringid(110000000,9)) then
 		e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_EFFECT)
-		e:GetHandler():RegisterFlagEffect(77631175,RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END,0,1)
 		return true
 	else return false end
 end
+function c77631175.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RaiseSingleEvent(e:GetHandler(),77631175,e,0,0,tp,nil)
+end
+function c77631175.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(500)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
+end
 function c77631175.atkop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.BreakEffect()
-	local e1=Effect.CreateEffect(e:GetHandler())
+	local c=e:GetHandler()
+	if not c:IsFaceup() or not c:IsRelateToEffect(e) then return end
+	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetReset(RESET_EVENT+0x1ff0000)
 	e1:SetValue(500)
 	e:GetHandler():RegisterEffect(e1)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Damage(1-tp,500,REASON_EFFECT)
+	Duel.Damage(p,d,REASON_EFFECT)
 end
