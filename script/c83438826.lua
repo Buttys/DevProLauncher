58f7ecea93e39e9f63ac13855fd730cd99fd7f0b
sidschingis <sijing@gmx.net>
2013-08-02 23:02:05 +0200

    fix

--- script/c83438826.lua
+++ script/c83438826.lua
@@ -80,15 +80,16 @@ function c83438826.desop(e,tp,eg,ep,ev,re,r,rp)
 	if not c:IsRelateToEffect(e) or not tc:IsRelateToEffect(e) or tc:IsFaceup() or  
 	not eq:IsAttackAbove(500)
 	then return end
-	
+	local atk=eq:GetAttack()
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(-500)
 	e1:SetReset(RESET_EVENT+0x1fe0000)
-	eq:RegisterEffect(e1)
-	
-	Duel.Destroy(tc,REASON_EFFECT)
+
+	if eq:RegisterEffect(e1) then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
 end
 function c83438826.eqcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
