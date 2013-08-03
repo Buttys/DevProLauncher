b2c38f9d5f393bb9f2f959cf2f2cb66daf03b698
sidschingis <sijing@gmx.net>
2013-08-03 21:40:56 +0200

    fix v2

--- script/c81907872.lua
+++ script/c81907872.lua
@@ -47,7 +47,8 @@ function c81907872.posop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c81907872.cfilter(c,tp)
-	return c:GetPreviousControler()==tp and c:IsSetCard(0x8d) and c:IsReason(REASON_DESTROY)
+	return c:GetPreviousControler()==tp and c:IsSetCard(0x8d) and c:IsReason(REASON_DESTROY) 
+		and (not c:IsReason(REASON_BATTLE) or c==Duel.GetAttackTarget())
 		and c:IsType(TYPE_MONSTER)
 end
 function c81907872.spcon(e,tp,eg,ep,ev,re,r,rp)
