097bcb6e4da3030d79c1deabeb0ecbc62e48549a
sidschingis <sijing@gmx.net>
2013-09-27 02:08:53 +0200

    fix

--- script/c80800048.lua
+++ script/c80800048.lua
@@ -1,28 +1,28 @@
 --Ｎｏ.３９ 希望皇ホープ・ルーツ
-function c??80800048.initial_effect(c)
+function c80800048.initial_effect(c)
 	--xyz summon
 	aux.AddXyzProcedure(c,aux.XyzFilterFunction(c,1),2)
 	c:EnableReviveLimit()
 	--disable attack
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(??80800048,0))
+	e1:SetDescription(aux.Stringid(80800048,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e1:SetCost(c??80800048.atkcost)
-	e1:SetTarget(c??80800048.atktg)
-	e1:SetOperation(c??80800048.atkop)
+	e1:SetCost(c80800048.atkcost)
+	e1:SetTarget(c80800048.atktg)
+	e1:SetOperation(c80800048.atkop)
 	c:RegisterEffect(e1)
 end
-function c??80800048.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+function c80800048.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
-function c??80800048.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+function c80800048.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetTargetCard(Duel.GetAttacker())
 end
-function c??80800048.atkop(e,tp,eg,ep,ev,re,r,rp)
+function c80800048.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and c:IsFaceup()
