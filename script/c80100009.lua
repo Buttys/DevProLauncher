3419fbd2e50a4104be6dba114a6d2b3c805c7f40
YuGiOhMoDDeR <admirshaggy@yahoo.com>
2013-11-17 17:51:42 +0100

    new
    the melody of awakening dragon

---
+++ script/c80100009.lua
@@ -0,0 +1,30 @@
+--ドラゴン・目覚めの旋律
+function c80100009.initial_effect(c)
+	--activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(c80100009.cost)
+	e1:SetTarget(c80100009.target)
+	e1:SetOperation(c80100009.operation)
+	c:RegisterEffect(e1)
+end
+function c80100009.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function c80100009.filter(c)
+	return c:IsRace(RACE_DRAGON) and c:IsAttackAbove(3000) and c:IsDefenceBelow(2500) and c:IsAbleToHand()
+end
+function c80100009.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(c80100009.filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function c80100009.operation(e,tp,eg,ep,ev,re,r,rp,chk)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,c80100009.filter,tp,LOCATION_DECK,0,1,2,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
