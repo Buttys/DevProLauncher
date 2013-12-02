f58441b99734f4e79c3cc892099e3b36bcdcf437
sidschingis <sijing@gmx.net>
2013-12-02 15:51:48 +0100

    fix

--- script/c80900001.lua
+++ script/c80900001.lua
@@ -35,6 +35,14 @@ function c80900001.condition(e,tp,eg,ep,ev,re,r,rp)
 end
 function c80900001.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetFlagEffect(tp,80900001)==0 end
+	--oath effects
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,0)
+	Duel.RegisterEffect(e1,tp)
 	Duel.RegisterFlagEffect(tp,80900001,RESET_PHASE+PHASE_END,EFFECT_FLAG_OATH,1)
 end
 function c80900001.spfilter(c,e,tp)
