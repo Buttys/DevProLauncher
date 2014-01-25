e259ec867839b28c92c4c3b7a70b7e83801c94e8
sidschingis <sijing@gmx.net>
2014-01-25 23:11:44 +0100

    Revert "temp"
    This reverts commit e2ce71a29d029a6555e0bc075e64b647c5089e01.

---
+++ script/c80200021.lua
@@ -0,0 +1,91 @@
+--森羅の賢樹 シャーマン
+function c80200021.initial_effect(c)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(80200021,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(c80200021.spcon)
+	e1:SetTarget(c80200021.sptg)
+	e1:SetOperation(c80200021.spop)
+	c:RegisterEffect(e1)
+	--deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(80200021,0))
+	e2:SetCategory(CATEGORY_TOGRAVE)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(c80200021.target)
+	e2:SetOperation(c80200021.operation)
+	c:RegisterEffect(e2)
+	--to hand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(80200021,2))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(c80200021.tdcon)
+	e3:SetTarget(c80200021.tdtg)
+	e3:SetOperation(c80200021.tdop)
+	c:RegisterEffect(e3)
+end
+function c80200021.cfilter(c)
+	return c:IsSetCard(0x90) and c:IsType(TYPE_MONSTER)
+end
+function c80200021.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(c80200021.cfilter,1,nil)
+end
+function c80200021.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c80200021.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+function c80200021.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 end
+end
+function c80200021.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)==0 then return end
+	Duel.ConfirmDecktop(tp,1)
+	local g=Duel.GetDecktopGroup(tp,1)
+	local tc=g:GetFirst()
+	if tc:IsRace(RACE_PLANT) then
+		Duel.DisableShuffleCheck()
+		Duel.SendtoGrave(tc,REASON_EFFECT+REASON_REVEAL)
+	else
+		Duel.MoveSequence(tc,1)
+	end
+end
+function c80200021.tdcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_DECK) and
+		(c:IsReason(REASON_REVEAL) or c:IsPreviousPosition(POS_FACEUP) or Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DECK))
+end
+function c80200021.filter(c)
+	return  c:IsSetCard(0x90) and c:IsType(TYPE_SPELL+TYPE_TRAP) and0c:IsAbleToHand()
+end
+function c80200021.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and c80200021.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c80200021.filter,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOHAND)
+	local g=Duel.SelectTarget(tp,c80200021.filter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function c80200021.tdop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SendtoHand(tc,nil,0,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
